eba31779734a78fac061f9c9a945466b2dde7278
AliJalil93 <ali.abdulzahraa@gmail.com>
2021-04-27 21:47:06 +0300

    1st commit

---
+++ app/src/main/java/com/ali/youtubelike/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ali.youtubelike
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
